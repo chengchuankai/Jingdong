516041d351fe7777e1373f036ca7018640b3a271
zhanghegang <31385023+zhanghegang@users.noreply.github.com>
2017-09-28 16:53:28 +0800

    one

--- app/src/main/java/zhanghegang/com/bawei/date0928jingdong/MyApp.java
+++ app/src/main/java/zhanghegang/com/bawei/date0928jingdong/MyApp.java
@@ -1,8 +1,14 @@
 package zhanghegang.com.bawei.date0928jingdong;
 
+import android.app.Application;
+
 /**
  * Created by asus on 2017/9/28.
  */
 
-public class MyApp {
+public class MyApp extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
